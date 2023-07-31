5fdbf42c8b5d7ccb9be56ab32603ca6e8196a461
scuty31 <87815827+scuty31@users.noreply.github.com>
2023-07-31 10:18:03 +0900

    Create A+B_4.py
    입력은 여러 개의 테스트 케이스로 이루어져 있다.
    
    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)

---
+++ Loop/A+B_4.py
@@ -0,0 +1,12 @@
+"""
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+"""
+
+import sys
+
+while True:
+    try:
+        A, B = map(int, sys.stdin.readline().rstrip().split())
+        print(A + B)
+    except:
+        break
