e381dc6eec8d0346b99c00fb83f72eb375dba355
Scuty <87815827+scuty31@users.noreply.github.com>
2026-08-10 09:44:21 +0900

    [level 0] Title: 배열 조각하기, Time: 4.79 ms, Memory: 12.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+def solution(arr, query):
+    answer = []
+    arr_queue = deque(arr)
+
+    for i in range(len(query)):
+        if i % 2 == 0:
+            while len(arr_queue) > query[i]+1:
+                arr_queue.pop()
+
+        else:
+            for _ in range(query[i]):
+                arr_queue.popleft()
+
+    answer = list(arr_queue)
+
+    return answer
