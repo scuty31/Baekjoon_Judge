9a868011bf5fcced4db65cf5d1c7eac1268d729d
Scuty <87815827+scuty31@users.noreply.github.com>
2026-08-20 11:07:01 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.34 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+def solution(t, p):
+    answer = 0
+    n = len(p)
+
+    end = n
+    num_list = deque(t[:n])
+
+    for i in range(n, len(t)):
+        if ''.join(num_list) <= p:
+            answer += 1
+
+        num_list.popleft()
+        num_list.append(t[i])
+
+    if ''.join(num_list) <= p:
+        answer += 1
+
+    return answer
