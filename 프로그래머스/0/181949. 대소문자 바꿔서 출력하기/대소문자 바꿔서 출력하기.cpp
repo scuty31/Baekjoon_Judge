c3069dff82d88c640711f7d40fa16f56fe2deaac
Scuty <87815827+scuty31@users.noreply.github.com>
2026-08-27 11:33:05 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 1.30 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(int i = 0; i<str.size(); i++){
+        if('A'<= str[i] && str[i] <= 'Z'){
+            str[i] = tolower(str[i]);
+        }
+        else{
+            str[i] = toupper(str[i]);
+        }
+    }
+    
+    cout << str;
+    
+    return 0;
+}
